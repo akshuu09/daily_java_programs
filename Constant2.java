1f60ef2d5d88ffd5c1f513d89830b52d35332ae6
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-02-16 22:32:57 +0530

    Add files via upload

---
+++ Constant2.java
@@ -0,0 +1,23 @@
+public class Constant2 
+{
+    public static void main(String args [])
+    {
+        int a=98;
+        switch(a)
+        {
+            case 10:
+            System.out.println("Inside case 10");
+            break;
+            case 'a':
+            System.out.println("Inside case a");
+            break;
+            case 'b':
+            System.out.println("Inside case b");
+            break;
+            default:
+            System.out.println("Inside default");
+
+        }
+    }
+}
+// a
