92fde83324b18a0496ef31bcacd194cb3a105e1a
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-02-17 22:07:36 +0530

    Add files via upload

---
+++ For3.java
@@ -0,0 +1,19 @@
+//WAJP to check if num 3 is existing betweeen 1 to 5 
+
+public class For3 
+{
+    public static void main(String args [])
+    {
+        for(int i=1; i<=5; i++)
+        {
+            System.out.println(i);
+        
+        if(i == 3)
+        {
+            System.out.println("the value of i is " +i);
+            break;
+        }
+    }
+    }
+    
+}
