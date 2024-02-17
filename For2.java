e6dcad0fe4f5618a072bfd0d62bfcc308b3c8928
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-02-17 22:06:33 +0530

    Add files via upload

---
+++ For2.java
@@ -0,0 +1,12 @@
+//WAJP to print num 1 to 5 in descending order 
+
+public class For2 
+{
+    public static void main(String args [])
+    {
+        for(int i=5; i>=1; i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
