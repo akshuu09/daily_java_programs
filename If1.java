070bb50f9eff607d040ad181ff669fe6963f2330
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-02-18 13:37:04 +0530

    Add files via upload

---
+++ If1.java
@@ -0,0 +1,10 @@
+public class If1 {
+    public static void main(String args[])
+    {
+        int a=10;
+        if(a>=10)
+        {
+            System.out.println("The num is greter");
+        }
+    }
+}
