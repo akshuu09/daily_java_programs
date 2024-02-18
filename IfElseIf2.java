b71dbc72dc57cb48bd6a812a6d028fcc661dcc78
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-02-18 13:38:23 +0530

    Add files via upload

---
+++ IfElseIf2.java
@@ -0,0 +1,23 @@
+public class IfElseIf2 
+{
+public static void main(String args [])
+{
+    char signal='g';
+    if(signal=='r')
+    {
+        System.out.println("stop");
+    }
+    else if(signal=='g')
+    {
+        System.out.println("Go");
+    }
+    else if(signal=='y')
+    {
+        System.out.println("Slow Downn");
+    }
+    else{
+        System.out.println("Invalid");
+    }
+
+}    
+}
