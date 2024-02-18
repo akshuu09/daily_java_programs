198b0c3792a38666a0559230b800b583b28e9fb8
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-02-18 13:42:47 +0530

    Add files via upload

---
+++ NumPattern.java
@@ -0,0 +1,19 @@
+public class NumPattern 
+{
+    public static void main(String args [])
+    {
+        for(int i=1; i<=4; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
+ // o/p
+ //1
+ //22
+ //333
+ //4444
