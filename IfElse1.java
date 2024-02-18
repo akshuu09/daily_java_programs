a9d48ef4ac76a4f74c4e7e8cc697e47f8f7e67ee
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-02-18 13:37:36 +0530

    Add files via upload

---
+++ IfElse1.java
@@ -0,0 +1,13 @@
+public class IfElse1 {
+    public static void main(String args[])
+    {
+        int age=21;
+        if(age >=20)
+        {
+            System.out.println("Elligible for driving");
+        }
+        else{
+            System.out.println("Not elligible for driving");
+        }
+    }
+}
