382ade15735bf2a4a0481cfed37e51b2dd7454b9
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-02-18 13:47:02 +0530

    Add files via upload

---
+++ NumPattern3.java
@@ -0,0 +1,17 @@
+public class NumPattern3 
+{
+    public static void main(String[] args)
+     {
+       
+        for(int i=1; i<=5; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                
+                System.out.print(" " +count+" ");
+                count ++;
+            }
+            System.out.println();
+        }
+    }
+}
