21859f9dcb998bfead82702eefc50b80e88f97f5
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-02-19 11:12:01 +0530

    Add files via upload

---
+++ Switch6.java
@@ -0,0 +1,23 @@
+//use of char,int,string
+
+public class Switch6 
+{
+    public static void main(String args [])
+    {
+        String str="success";
+        switch(str)
+        {
+            case'a':
+            System.out.println("Inside case a");
+            break;
+            case "success":
+            System.out.println("Inside success");
+            break;
+            case 1:
+            System.out.println("Inside case 1");
+            break;
+            default:
+            System.out.println("Inside default");
+        }
+    }
+}
