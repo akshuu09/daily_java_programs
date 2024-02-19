367b3a1734eb960c8612bf1980e95b77bff4451a
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-02-19 11:11:04 +0530

    Add files via upload

---
+++ Switch4.java
@@ -0,0 +1,24 @@
+public class Switch4
+ {
+    public static void main(String  args[])
+    {
+        int a=3;
+        switch(a)
+
+        {
+            case 1+1:
+            System.out.println("Inside case 1");
+            break;
+            case 2+1:
+            System.out.println("Inside case 2");
+            break;
+            case 3+1:
+            System.out.println("Inside case 3");
+            break;
+            default:
+            System.out.println("Inside default");
+        }
+    }
+    
+}
+//expressions can be use a valid case  labels
