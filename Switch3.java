2d8959cd354ac2ea442508bca7c60745101a7f86
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-02-19 11:10:16 +0530

    Add files via upload

---
+++ Switch3.java
@@ -0,0 +1,14 @@
+public class Switch3 
+{
+    public static void main(String args [])
+    {
+        int a=2;
+        switch (a)
+        {
+            default:
+            System.out.println("Inside default");
+        }
+
+    }
+}
+//default is not mandatory within switch statement
