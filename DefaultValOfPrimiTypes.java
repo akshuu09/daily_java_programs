ff6ff60ba39985ff68b4c4be9d708f61e3e44400
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-02-21 09:21:17 +0530

    Add files via upload

---
+++ DefaultValOfPrimiTypes.java
@@ -0,0 +1,23 @@
+public class DefaultValOfPrimiTypes
+ {
+    static int a;
+    static float f;
+    static char c;
+    static short s;
+    static long l;
+    static double d;
+    static boolean b;
+    static byte k;
+
+    public static void main(String args [])
+    {
+        System.out.println(a);
+        System.out.println(f);
+        System.out.println(c);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(k);
+    }
+}
