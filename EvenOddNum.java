f92f5d2f5ba4cd71904a4cb37848a06147abf797
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-02-21 09:21:51 +0530

    Add files via upload

---
+++ EvenOddNum.java
@@ -0,0 +1,15 @@
+public class EvenOddNum 
+{
+    public static void main(String args [])
+    {
+        int num=10;
+        if(num %2 ==0)
+        {
+            System.out.println("The number is even");
+
+        }
+        else{
+            System.out.println("The number is odd");
+        }
+    }
+}
