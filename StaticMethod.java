65fb842a8cb88d5bdf2554f6f877ecd10e80df19
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-02-22 22:21:51 +0530

    Add files via upload

---
+++ StaticMethod.java
@@ -0,0 +1,11 @@
+public class StaticMethod {
+    
+    public static void main(String[] args) {
+        StaticMethod.method1();
+
+    }
+    static void method1(){
+        System.out.println("Inside method");
+        return;
+    }
+}
