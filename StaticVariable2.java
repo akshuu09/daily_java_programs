3acad5399c3f5de7c911812b2942c016785272ef
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-02-22 22:22:56 +0530

    Add files via upload

---
+++ StaticVariable2.java
@@ -0,0 +1,10 @@
+public class StaticVariable2 
+{
+    static int num=123;
+    public static void main(String args [])
+    {
+        //using classs name
+        System.out.println(StaticVariable2.num);
+
+    }
+}
