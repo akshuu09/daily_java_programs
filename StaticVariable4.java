0b71813625b454c0e5057320e10bcd5707a4716f
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-02-22 22:23:47 +0530

    Add files via upload

---
+++ StaticVariable4.java
@@ -0,0 +1,11 @@
+public class StaticVariable4 {
+    static int i=12;
+    static float f=12.4f;
+    public static void main(String[] args) {
+        float result;
+    
+        result=i+f;
+        System.out.println(result);
+    }
+
+}
