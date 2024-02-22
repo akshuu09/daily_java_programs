23778b66e7cec42827255943d0a5ebfe17d1a78d
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-02-22 22:27:10 +0530

    Add files via upload

---
+++ StaticVariable5.java
@@ -0,0 +1,9 @@
+public class StaticVariable5 {
+    static float result;
+    public static void main(String[] args) {
+        int a=10;
+        float b=10.5f;
+StaticVariable5.result=a+b;
+System.out.println(result);
+    }
+}
