9528e94528b455885c80c3ce44e628bab2ace033
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-02-22 22:26:26 +0530

    Add files via upload

---
+++ StaticMethod4.java
@@ -0,0 +1,11 @@
+class StaticMethod4
+ {
+    public static void main(String[] args) {
+        int a=100;
+        m1(a);
+    }
+    static void m1(int a)
+    {
+        System.out.println(a);
+    }
+}
