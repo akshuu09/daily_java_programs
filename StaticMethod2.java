c2ea6d49bbc085c7694ee2423a4ab3f72d083e0b
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-02-22 22:25:16 +0530

    Add files via upload

---
+++ StaticMethod2.java
@@ -0,0 +1,10 @@
+public class StaticMethod2 {
+    static int a=100;
+    public static void main(String[] args) {
+         float b=200.5f;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(StaticMethod2.a);
+       
+    }
+}
