3eb8727b7fd20be3762f75b3dbe6998f8b5f750a
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-02-23 22:55:15 +0530

    Add files via upload

---
+++ NonStaticMethod1.java
@@ -0,0 +1,14 @@
+public class NonStaticMethod1 {
+    public static void main(String[] args) {
+        NonStaticMethod1 obj1 = new NonStaticMethod1();
+        obj1.m1();
+        
+    }
+        void m1(){
+            System.out.println("Inside m1 method");
+        }
+    }
+
+        
+    
+
