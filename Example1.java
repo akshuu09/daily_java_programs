75782d3ad7cf589c6a4c7f2584a2ee285782cc38
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-04-18 22:25:33 +0530

    Add files via upload

--- Example1.java
+++ Example1.java
@@ -1,28 +1,9 @@
-class Demo
+abstract class Example1
 {
-	int a=10;
-	static float b=20.5f;
-	void m2()
-	{
-		System.out.println("In Demo class -m2 method");
-		Sample s1=new Sample();
-		s1.m1();
-	}
-	static class Sample
-	{
-		void m1()
-		{
-			System.out.println("In static class sample-m1 method");
-			Demo dd=new Demo();
-			System.out.println(dd.a+b);
-		}
-	}
-}
-class Example1
-{
+	abstract void method();
 	public static void main(String args[])
 	{
-		Demo d1=new Demo();
-		d1.m2();
+		System.out.println("Start....");
+		System.out.println("....Stop");
 	}
 }
