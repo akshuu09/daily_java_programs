1fd4442f999b85324101a21bef32b00a3ea06304
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-04-22 09:53:30 +0530

    Add files via upload

---
+++ StringClasses/Example2.java
@@ -0,0 +1,12 @@
+class Example2
+{
+	public static void main(String args[])
+	{
+		String str1="Java";
+		String str2="Programming";
+		String str3="Java";
+		System.out.println(str1==str2);
+		System.out.println(str2==str3);
+		System.out.println(str1==str3);
+	}
+}
