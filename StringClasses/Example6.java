cab7e9cef4c1c1c32b8b9eaae6f143c200fa08c3
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-04-23 22:16:25 +0530

    Add files via upload

---
+++ StringClasses/Example6.java
@@ -0,0 +1,10 @@
+class Example6
+{
+	public static void main(String args[])
+	{
+		StringBuffer sb=new StringBuffer("Success");
+		System.out.println(sb);
+		System.out.println(sb.toString());//toString-->StringBuffer class
+	}
+}
+//public java.lang.StringBuffer(java.lang.String);
