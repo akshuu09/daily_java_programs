190dba07dc17fb057cd80f3fbaedb72917980d06
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-04-23 22:17:56 +0530

    Add files via upload

---
+++ StringClasses/Example9.java
@@ -0,0 +1,18 @@
+class Example9
+{
+	
+
+	public static void main(String args[])
+	{
+		String str="Cyber";
+		System.out.println(str);
+		StringBuffer sb=new StringBuffer(str);
+		System.out.println(sb);
+		sb.append("Traning");
+		System.out.println(sb);
+
+	}
+}
+//String to StringBuffer--->Constructor
+//public java.lang.StringBuffer(java.lang.String)
+//public java.lang.StringBuffer append(java.lang.String)
