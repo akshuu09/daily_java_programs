122d73c11be8c0ea2b259ae8d4a35ff4b340e4ca
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-04-24 14:56:21 +0530

    Add files via upload

---
+++ StringClasses/Example12.java
@@ -0,0 +1,12 @@
+class Example12
+{
+	public static void main(String args[])
+	{
+		String str1="cyber";
+		String str2="Cyber";
+		System.out.println(str1.compareTo(str2));//0--->32
+		System.out.println(str1.equals(str2));//true--->false
+	}
+}
+//a-->97
+//A-->65
