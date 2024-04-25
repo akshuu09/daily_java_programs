3552f6837c6a9c4db6bb04b5b2c684cde54c1eb1
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-04-25 14:53:01 +0530

    Add files via upload

---
+++ StringClasses/Example15.java
@@ -0,0 +1,15 @@
+class Example15
+{
+	public static void main(String args[])
+	{
+		String username="ShindeAkshta";
+		if(username.length()>=7)
+		{
+			System.out.println("Username Entered Is Correct");
+		}
+		else
+		{
+			System.out.println("Username Entered Is Not Correct");
+		}
+	}
+}
