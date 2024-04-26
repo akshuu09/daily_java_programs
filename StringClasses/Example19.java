947b25d18506a4e25f882425648ef146631c00fa
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-04-26 21:11:31 +0530

    Add files via upload

---
+++ StringClasses/Example19.java
@@ -0,0 +1,12 @@
+class Example19
+{
+	public static void main(String args[])
+	{
+		String str="CyberSuccessTraining";
+		System.out.println(str.substring(0,4));
+		System.out.println(str.substring(5,11));
+		System.out.println(str.substring(12,20));
+		System.out.println(str.substring(0,20));
+
+	}
+}
