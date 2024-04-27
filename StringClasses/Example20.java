e6d0f398b963ed075584bc07e1c65d33a6e5f176
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-04-27 22:38:58 +0530

    Add files via upload

---
+++ StringClasses/Example20.java
@@ -0,0 +1,12 @@
+class Example20
+{
+	public static void main(String args[])
+	{
+		int[] a={10,20,30};
+		for(int i=0; i<=2; i++)
+		{
+			System.out.println(" "+a[i]);
+		}
+	}
+}
+//public String[] split(String regex)
