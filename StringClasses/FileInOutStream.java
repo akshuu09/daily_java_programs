aadd5020a79414d03afc7cd3c65862197faf594c
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-04-28 22:25:06 +0530

    Add files via upload

---
+++ StringClasses/FileInOutStream.java
@@ -0,0 +1,8 @@
+import java.io.FileInputStream;
+class FileInOutStream
+{
+	public static void main(String args[])
+	{
+		FileInputStream fis=new FileInputStream("D:\\Programming\\Source1.txt");
+	}
+}
