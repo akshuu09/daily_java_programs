49967fee76403993e805eea790eb15f0b672d1b0
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-04-30 21:25:12 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,24 @@
+class Pattern3
+{
+	public static void main(String args[])
+	{
+		for(int row=5; row>0; row--)
+		{
+			for(int col=0; col<row; col++)
+			{
+				System.out.print("*");
+			}
+			
+			 System.out.println();
+		}
+		
+	}
+}
+
+/*----o/p 
+
+*****
+****
+***
+**
+*
