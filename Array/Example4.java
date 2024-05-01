e6fa6e5199aa233813999e0266c3215e8538fe70
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-05-01 23:06:54 +0530

    Add files via upload

---
+++ Array/Example4.java
@@ -0,0 +1,14 @@
+class Example4
+{
+	public static void main(String args[])
+	{
+		String []s=new String[3];
+		s[0]="Cyber";
+		s[1]="Success";
+		s[2]="Traning";
+		for(int i=0; i<s.length; i++)
+		{
+			System.out.println(s[i]);
+		}
+	}
+}
