6035fe077e49c626aa38ba7a3f802193e431101f
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-05-01 22:53:59 +0530

    Add files via upload

---
+++ Array/Example2.java
@@ -0,0 +1,12 @@
+class Example2
+{
+	public static void main(String args[])
+	{
+		int []a=new int[5];
+		System.out.println(a.length);
+		for(int i=0; i<a.length; i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+}
