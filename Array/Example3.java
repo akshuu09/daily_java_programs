b3bfaf652d29ccadd29fc3966d7c1358120f5869
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-05-01 23:02:07 +0530

    Add files via upload

---
+++ Array/Example3.java
@@ -0,0 +1,21 @@
+class Example3
+{
+	public static void main(String args[])
+	{
+		float []f1=new float[3];
+		for(int i=0; i<f1.length; i++)
+		{
+			System.out.println(f1[i]);
+		}
+		char[] c1=new char[3];
+		for(int i=0; i<c1.length; i++)
+		{
+			System.out.println(c1[i]);
+		}
+		boolean []b1=new boolean[3];
+		for(int i=0; i<b1.length; i++)
+		{
+			System.out.println(b1[i]);
+		}
+	}
+}
