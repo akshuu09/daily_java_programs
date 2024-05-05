1da1822cd4b14a95efea13b95d20bb6e03f7a1cd
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-05-05 20:56:33 +0530

    Add files via upload

---
+++ Array/Example20.java
@@ -0,0 +1,11 @@
+class Example20
+{
+	public static void main(String args[])
+	{
+		int[][] a={{100,200,300},
+			       {400,500,600}};
+
+		System.out.println(a[0][2]);
+		System.out.println(a[1][2]);
+	}
+}
