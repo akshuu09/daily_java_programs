dcebbb79324a4a498fc078a747654bcb8bd94c76
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-05-08 21:42:16 +0530

    Add files via upload

---
+++ Abstraction/Example3.java
@@ -0,0 +1,11 @@
+
+abstract class Example3
+{
+	public static void main(String agrs[])
+	{
+		System.out.println("Stop....");
+		Example3 e3=new Example3();
+		System.out.println("....Stop");
+	}
+}
+// we can  not create object of abstract class.
