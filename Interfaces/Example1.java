55f16c225f72a0c8e8c43aab11787d35fe790b41
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-05-10 14:31:44 +0530

    Update Example1.java

--- Interfaces/Example1.java
+++ Interfaces/Example1.java
@@ -1,22 +1,18 @@
-abstract class Sample
-{
-	abstract void demo();
-}
-class Abstract extends Sample
-{
-	void demo()
+interface Sample
 	{
-		System.out.println("In method demo");
+		void m1();
 	}
-}
 class Example1 
 {
-	
+	public void m1()
+	{
+		System.out.println("Inside m1 method");
+	}
 	public static void main(String args[])
 	{
 		System.out.println("Start.....");
-		Abstract  as=new Abstract();
-		as.demo();
+	        Example1 e1=new Example1();
+		e1.m1();
 		System.out.println(".....Stop");
 		
 	}
