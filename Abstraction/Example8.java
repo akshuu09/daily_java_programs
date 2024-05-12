3bcebcf4fe6c99de0117eb74e7c6ab612e8d0ff5
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-05-12 10:09:19 +0530

    Add files via upload

---
+++ Abstraction/Example8.java
@@ -0,0 +1,13 @@
+abstract class Class1
+{
+}
+class Example8
+{
+	public static void main(String args[])
+	{
+		System.out.println("Star....");
+		System.out.println("....Stop");
+	}
+}
+/*if there is no member within the  abstract class the 
+there is no implementation require within the child class */
