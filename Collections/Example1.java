dcd7c9105e154e6601fe90e971895adc2a9af888
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-05-15 16:49:25 +0530

    Add files via upload

---
+++ Collections/Example1.java
@@ -0,0 +1,16 @@
+package collectionsProgram;
+
+import java.util.ArrayList;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		ArrayList al=new ArrayList();
+		al.add(100);
+		al.add("collection");
+		al.add('A');
+		al.add(100);
+		System.out.println(al);
+	}
+
+}
