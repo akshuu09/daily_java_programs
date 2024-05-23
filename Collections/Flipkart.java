49e458941e2e1b4a305d9f0af83ab757c11e0c86
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-05-23 16:54:35 +0530

    Add files via upload

---
+++ Collections/Flipkart.java
@@ -0,0 +1,19 @@
+package collectionsProgram;
+
+import java.util.Comparator;
+
+public class Flipkart 
+{
+    int itemPrice;
+    String itemName;
+    Flipkart(int itemPrice,String itemName)
+    {
+    	this.itemPrice=itemPrice;
+    	this.itemName=itemName;
+    }
+	
+		
+	
+	
+    
+}
