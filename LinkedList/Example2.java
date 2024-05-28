c42f6097579d39ccd71d49de2109578e9d4e65c2
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-05-28 09:38:00 +0530

    Add files via upload

---
+++ LinkedList/Example2.java
@@ -0,0 +1,19 @@
+package linkedList;
+
+import java.util.LinkedList;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		LinkedList<String> l1=new LinkedList<String>();
+		l1.add("A");
+		l1.add("B");
+		l1.add("C");
+		System.out.println(l1);
+		
+		l1.add("Cyber");
+		l1.add("Security");
+		System.out.println(l1);
+	}
+
+}
