625a3d1e114ebb44ffd3673d854cb89e0936f9a7
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-05-28 09:37:37 +0530

    Add files via upload

---
+++ LinkedList/Example1.java
@@ -0,0 +1,18 @@
+package linkedList;
+
+import java.util.LinkedList;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		LinkedList l1=new LinkedList();
+		l1.add("Hello Friends..");
+		l1.add(2024);
+		l1.add('S');
+		l1.add("IT");
+		l1.add(null);
+		l1.add(10.5f);
+		System.out.println(l1);
+	}
+
+}
