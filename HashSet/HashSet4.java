28ee04f327069eea8268c0ef9d93612f0cebdf00
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-06-02 11:49:37 +0530

    Add files via upload

---
+++ HashSet/HashSet4.java
@@ -0,0 +1,16 @@
+package hashSet;
+
+import java.util.HashSet;
+
+public class HashSet4 {
+
+	public static void main(String[] args) 
+	{
+		HashSet hs1=new HashSet<>(20,10.5f);
+		hs1.add(100);
+		hs1.add(200);
+		hs1.add(300);
+		System.out.println(hs1);
+	}
+
+}
