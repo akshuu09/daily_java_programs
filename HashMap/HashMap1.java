1b15125894f19f8477ae1aa5464c28d0d9f90672
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-06-05 16:20:30 +0530

    Add files via upload

---
+++ HashMap/HashMap1.java
@@ -0,0 +1,16 @@
+package hashMap;
+
+import java.util.HashMap;
+
+public class HashMap1 {
+
+	public static void main(String[] args) {
+		HashMap hm1=new HashMap();
+		hm1.put(101,'A');
+		hm1.put(102,'B');
+		hm1.put(103,'C');
+		hm1.put(104,'D');
+		System.out.println(hm1);
+	}
+
+}
