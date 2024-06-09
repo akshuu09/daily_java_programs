2e746c911579eb0adb4ea54154585d16e51c0ea0
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-06-09 18:29:05 +0530

    Add files via upload

---
+++ TreeSet/TreeSet1.java
@@ -0,0 +1,17 @@
+package treeSet;
+
+import java.util.TreeSet;
+
+public class TreeSet1 {
+
+	public static void main(String[] args) {
+		TreeSet ts1=new TreeSet();
+		ts1.add(100);
+		ts1.add(300);
+		ts1.add(400);;
+		ts1.add(5000);
+		System.out.println(ts1);
+				
+	}
+
+}
