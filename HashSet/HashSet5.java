338bed30d13017a9bfe289fe263680aee93b7cee
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-06-25 21:43:01 +0530

    Add files via upload

---
+++ HashSet/HashSet5.java
@@ -0,0 +1,20 @@
+package hashSet;
+
+import java.util.HashSet;
+
+public class HashSet5 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		HashSet<String> cars = new HashSet<String>();
+	    cars.add("Volvo");
+	    cars.add("BMW");
+	    cars.add("Ford");
+	    cars.add("BMW");
+	    cars.add("Mazda");
+	    System.out.println(cars);
+
+	}
+
+}
