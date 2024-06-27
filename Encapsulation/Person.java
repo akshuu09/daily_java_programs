e8e1bdc48bc73515fde5e27db91a754d4e9dcf1d
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-06-27 21:16:22 +0530

    Add files via upload

---
+++ Encapsulation/Person.java
@@ -0,0 +1,15 @@
+package encapsulation;
+
+public class Person {
+	  String name; // private = restricted access
+
+	  // Getter
+	  public String getName() {
+	    return name;
+	  }
+
+	  // Setter
+	  public void setName(String newName) {
+	    this.name = newName;
+	  }
+}
