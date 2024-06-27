00b64ce4a08a5ef42d11edd5d00cb68384cf22a5
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-06-27 21:15:57 +0530

    Add files via upload

---
+++ Encapsulation/Example1.java
@@ -0,0 +1,12 @@
+package encapsulation;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		Person myObj = new Person();
+	    myObj.name = "Krishna";  // error
+	    System.out.println(myObj.name); // error 
+	  }
+	}
+
+
