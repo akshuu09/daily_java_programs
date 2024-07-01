95af8098fb6708fb362b77e4305843cd0f241ddc
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-07-01 22:02:16 +0530

    Add files via upload

---
+++ Thread/Example1.java
@@ -0,0 +1,18 @@
+package thread;
+
+public class Example1 extends Thread {
+
+	public static void main(String[] args) {
+		
+			  
+			    Example1 thread = new Example1();
+			    thread.start();
+			    System.out.println("This code is outside of the thread");
+			  }
+			  public void run() {
+			    System.out.println("This code is running in a thread");
+			  }
+			
+	}
+
+
