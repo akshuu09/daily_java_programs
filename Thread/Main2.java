ad9bd5816c10ea5586265e0c62f4355ec510be8b
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-07-02 21:20:53 +0530

    Add files via upload

---
+++ Thread/Main2.java
@@ -0,0 +1,22 @@
+package thread;
+
+public class Main2 extends Thread {
+	  public static int amount = 0;
+
+	  public static void main(String[] args) {
+	    Main thread = new Main();
+	    thread.start();
+	    // Wait for the thread to finish
+	    while(thread.isAlive()) {
+	      System.out.println("Waiting...");
+	    }
+	    // Update amount and print its value
+	    System.out.println("Main: " + amount);
+	    amount++;
+	    System.out.println("Main: " + amount);
+	  }
+	  public void run() {
+	    amount++;
+	  }
+	}
+
