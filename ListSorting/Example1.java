8e9eb246c2ac7fe1906e3beebc9ace0b4877c88e
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-07-03 22:31:07 +0530

    Add files via upload

---
+++ ListSorting/Example1.java
@@ -0,0 +1,22 @@
+package listSorting;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		 ArrayList<String> cars = new ArrayList<String>();
+		    cars.add("Volvo");
+		    cars.add("BMW");
+		    cars.add("Ford");
+		    cars.add("Mazda");
+
+		    Collections.sort(cars);  // Sort cars
+
+		    for (String i : cars) {
+		      System.out.println(i);
+	}
+
+}
+}
