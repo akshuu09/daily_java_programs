9cb1512322c9ac1237a2036a0868aa19aabdbba6
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-07-07 20:45:31 +0530

    Add files via upload

---
+++ HashSet/Example6.java
@@ -0,0 +1,25 @@
+package hashSet;
+
+import java.util.HashSet;
+
+public class Example6 {
+
+	public static void main(String[] args) {
+		HashSet<Integer> numbers = new HashSet<Integer>();
+
+	    // Add values to the set
+	    numbers.add(4);
+	    numbers.add(7);
+	    numbers.add(8);
+
+	    // Show which numbers between 1 and 10 are in the set
+	    for(int i = 1; i <= 10; i++) {
+	      if(numbers.contains(i)) {
+	        System.out.println(i + " was found in the set.");
+	      } else {
+	        System.out.println(i + " was not found in the set.");
+	      }
+	    }
+	}
+
+}
