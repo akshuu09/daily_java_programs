1bb69ad640d237dbfe0e59ed8cb2ab570b5ca3a5
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-07-07 20:47:36 +0530

    Add files via upload

---
+++ HashMap/Example7.java
@@ -0,0 +1,18 @@
+package hashMap;
+
+import java.util.HashMap;
+
+public class Example7 {
+
+	public static void main(String[] args) {
+		HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+	    // Add keys and values (Country, City)
+	    capitalCities.put("England", "London");
+	    capitalCities.put("Germany", "Berlin");
+	    capitalCities.put("Norway", "Oslo");
+	    capitalCities.put("USA", "Washington DC");
+	    System.out.println(capitalCities);
+	}
+
+}
