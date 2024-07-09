d0794c8158b7bc989c630efccadc5a01a4e7fcf5
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-07-09 17:22:57 +0530

    Add files via upload

---
+++ LambdaExp/Example1.java
@@ -0,0 +1,16 @@
+package lambdaExp;
+
+import java.util.ArrayList;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+	    numbers.add(5);
+	    numbers.add(9);
+	    numbers.add(8);
+	    numbers.add(1);
+	    numbers.forEach( (n) -> { System.out.println(n); } );
+	}
+
+}
