bbe62714d18bcf291f89ea7dba74f276b0f779c7
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-08-06 11:25:51 +0530

    Add files via upload

---
+++ patternss/Pattern1.java
@@ -0,0 +1,22 @@
+/*
+*
+**
+***
+****
+*****
+*/
+
+class Pattern1
+{
+	public static void main(String args[])
+	{
+		for(int row=0; row<=5; row++)
+		{
+			for(int col=0; col<row; col++)
+			{
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+	}
+}
