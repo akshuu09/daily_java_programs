06e2ad378f5bd78cb8263b8d51910b73c51e7af0
Shinde Akshta <144618084+akshuu09@users.noreply.github.com>
2024-09-12 20:17:51 +0530

    Add files via upload

---
+++ ConvertNumToWord.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class ConvertNumToWord
+{
+	s
+	public static void main(String args[])
+	{
+		
+		Scanner as =new Scanner (System.in);
+		System.out.print("Enter the number:");
+		
+		}
+	}
+}
